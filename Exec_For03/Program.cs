33d02ad805822c4d22e68f2aab716c40dbacc19b
xiuwei <xiuwei@DESKTOP-CIM4AC6>
2022-10-15 22:22:45 -0700

    加入專案檔案。

---
+++ Exec_For03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For03
+{
+    /// <summary>
+    /// 右側對齊星形三角形
+    ///     *
+    ///    **
+    ///   ***
+    ///  ****
+    /// *****
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                int countempty = 6 - i;
+                int countstar = i;
+
+                string row = new string(' ', countempty) + new string('*', countstar);
+                Console.WriteLine(row);
+
+            }
+
+        }
+    }
+}
